ba172a3c89e79f2edef227c8a7133b2937b263ff
sreekanthpaul <sreekanthpaul@gmail.com>
2025-05-03 23:10:17 +0530

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+//PascalCasing
+function Message(){
+    //JSX: JavaScript XML
+    const God = 'Jesus';
+    return <><h4>Hello {God} Love in Every Person -- Super</h4><div><input type="text"/></div></>;
+}
+
+export default Message;
